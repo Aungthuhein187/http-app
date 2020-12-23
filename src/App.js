9a18170c98c4fa2a373e9871d6b5050709b1c732
Aungthuhein187 <aungthuhein187@gmail.com>
2020-12-23 16:16:49 +0630

    call backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+const config = 'https://jsonplaceholder.typicode.com/posts';
 
 class App extends Component {
   state = { posts: [] };
+
+  componentDidMount() {}
+
+  handleAdd = () => {
+    console.log('Post added.');
+  };
+
+  handleUpdate = (post) => {
+    console.log('Post updated.', post);
+  };
+
+  handleDelete = (post) => {
+    console.log('Post deleted.', post);
+  };
+
   render() {
+    const { posts } = this.state;
+
     return (
       <div className="container">
-        <button className="btn btn-primary">Add</button>
+        <button className="btn btn-primary btn-add" onClick={this.handleAdd}>
+          Add
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post) => (
+              <tr>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => this.handleUpdate(post)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => this.handleDelete(post)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
